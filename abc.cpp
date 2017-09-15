30067bfd96d5312220623c29d23e93f9ae0c09ab
Nusrat <31994644+Nusrat205@users.noreply.github.com>
2017-09-15 23:48:32 +0600

    first_commit

---
+++ abc.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+cout<<"this is me">>end1;
+return 0;
+}
---
+++ example.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+  string a,b,c;
+  a="nusrat ";
+  b="go on";
+  c=a+b;
+  cout<<c<<endl;
+
+}
